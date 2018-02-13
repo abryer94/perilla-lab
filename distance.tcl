c26fc510055e8eec469400bbed7a56cba3edd8a4
abryer94 <32651550+abryer94@users.noreply.github.com>
2018-02-13 12:08:26 -0500

    Update distance.tcl

--- distance.tcl
+++ distance.tcl
@@ -3,14 +3,20 @@
 #### AJB - Tcl script to format distance restraint, colvars file
 ####	   for NAMD. Parsed from talos
 
-if { $argc != 2 } { 
-	puts "usage: tclsh $argv0 \$inputfile \$outputfile"
+#!/bin/tclsh 
+#
+#### AJB - Tcl script to format distance restraint, colvars file
+####	   for NAMD. Parsed from talos
+
+if { $argc != 3 } { 
+	puts "usage: tclsh $argv0 \$inputfile \$outputfile segment ID"
 	exit
 }
 
 #input and output; read/write 
 set input [open [lindex $argv 0] r] 
 set output [open [lindex $argv 1] w]
+set psfSegID [lindex $argv 2]
 
 #set force constant
 set k 200.0
@@ -28,19 +34,25 @@ while { [gets $input line] != -1 } {
 
 		puts $output "colvar { " 
 		puts $output "\t name distance:$resid1-$resid2"
-		puts $output "\t lowerWalls $lower"
-		puts $output "\t upperWalls $upper"
-		puts $output "\n\t distance { "
-		puts $output "\t\t group1 {atomNameResidueRange $atmname1 $resid1-$resid1} "
-		puts $output "\t\t group2 {atomNameResidueRange $atmname2 $resid2-$resid2} "
+		puts $output "\t lowerWall $lower"
+		puts $output "\t upperWall $upper"
+		puts $output "\t lowerWallConstant $k"
+		puts $output "\t upperWallConstant $k"
+		puts $output "\t distance { "
+		puts $output "\t\t group1 { "
+		puts $output "\t\t psfSegID $psfSegID"
+		puts $output "\t\t atomNameResidueRange $atmname1 $resid1-$resid1 } "
+		puts $output "\t\t group2 { "
+		puts $output "\t\t psfSegID $psfSegID"
+		puts $output "\t\t atomNameResidueRange $atmname2 $resid2-$resid2 } "
 		puts $output "\t\t} "
 		puts $output "} "
 
-		puts $output "\nharmonicWalls { "
-		puts $output "\tcolvars distance:$resid1-$resid1"
-		puts $output "\t lowerWallConstant $k "
-		puts $output "\t upperWallConstant $k "
-		puts $output "} \n"
+		#puts $output "\nharmonicWalls { "
+		#puts $output "\tcolvars distance:$resid1-$resid1"
+		#puts $output "\t lowerWallConstant $k "
+		#puts $output "\t upperWallConstant $k "
+		#puts $output "} \n"
 	 
 	}
 }
